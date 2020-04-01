8f4bb26a6ccca15ec611bbd878af5f5f28303426
Dilip Krishnan <D_KRISHNAN@containerstore.com>
2020-03-31 19:44:04 -0500

    Added better check around file permissions

--- weather-data-loader/src/main/java/com/dilipkrish/weather/dataloader/services/GZipFileTasklet.java
+++ weather-data-loader/src/main/java/com/dilipkrish/weather/dataloader/services/GZipFileTasklet.java
@@ -1,11 +1,13 @@
 package com.dilipkrish.weather.dataloader.services;
 
+import com.dilipkrish.weather.dataloader.BatchConfiguration;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -24,25 +26,31 @@ public class GZipFileTasklet implements Tasklet {
     }
 
     @Override
-    public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+    public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) {
         byte[] buffer = new byte[1024];
 
         try {
 
             log.info("Uncompressing file: {} to {}...", gzipSourceFilePath, uncompressedFilePath);
-            GZIPInputStream gzis =
-                    new GZIPInputStream(new FileInputStream(gzipSourceFilePath));
 
-            FileOutputStream out =
-                    new FileOutputStream(uncompressedFilePath);
+            File downloadFolder = new File(BatchConfiguration.DOWNLOAD_FOLDER);
+            if (downloadFolder.canWrite()) {
+                GZIPInputStream gzis =
+                        new GZIPInputStream(new FileInputStream(gzipSourceFilePath));
 
-            int len;
-            while ((len = gzis.read(buffer)) > 0) {
-                out.write(buffer, 0, len);
+                FileOutputStream out =
+                        new FileOutputStream(uncompressedFilePath);
+
+                int len;
+                while ((len = gzis.read(buffer)) > 0) {
+                    out.write(buffer, 0, len);
+                }
+
+                gzis.close();
+                out.close();
+            } else {
+                log.warn("No permissions to write to folder {}, skipping this step", downloadFolder.toPath());
             }
-
-            gzis.close();
-            out.close();
         } catch (IOException ex) {
             log.error("Error unzipping file", ex);
         }
